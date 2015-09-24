2a81e8c4dc0ad8fc677e63342ec2ba6626bedb7c
Tiny Giant <ajsa.ferguson@gmail.com>
2015-09-24 13:48:09 -0600

    Update editor.user.js

--- editor.user.js
+++ editor.user.js
@@ -9,9 +9,9 @@
 // @grant          none
 // @license        MIT
 // @namespace      http://github.com/AstroCB
-// @version        1.5.2.23
+// @version        1.5.2.24
 // @description    Fix common grammar/usage annoyances on Stack Exchange posts with a click
-// @include        /^https?://(meta\.)?(stackoverflow|stackexchange|serverfault|superuser|askubuntu|stackapps)\.com/(questions|posts|review)/(?!tagged|new).*/
+// @include        /^https?://\w*.?(stackoverflow|stackexchange|serverfault|superuser|askubuntu|stackapps)\.com/(questions|posts|review)/(?!tagged|new).*/
 // ==/UserScript==
 
 (function() {
